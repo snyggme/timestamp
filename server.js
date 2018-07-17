ddeb06b5239332d707fcaa86a745fa37c39416a2
Glitch (peach-okra) <none>
2018-07-17 18:31:04 +0000

    📮⚡️ Checkpoint ./package.json:1469898/30 ./server.js:1469898/263

--- server.js
+++ server.js
@@ -4,11 +4,17 @@
 // init project
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
+  next()
+})
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,6 +27,11 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get("/api/timestamp/:date_string", function (req, res) {
+  
   res.json({greeting: 'hello API'});
 });
 
